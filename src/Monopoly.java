48fc32a829b2c436b726d357c392542b33d160ad
ritvi <ritvi@Ritviks-dell>
2020-05-11 15:25:09 -0700

    hehe

---
+++ src/Monopoly.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Monopoly {
+public static void main(String[]args) {
+	
+	int location1 = 0;
+	int location2 = 0;
+	
+	Scanner myObj = new Scanner(System.in);//For scanning what the user inputed
+	System.out.println("Welcome to Monopoly");
+	String start = myObj.nextLine();
+	System.out.println("It is the first player's turn");
+	System.out.println("Press any Number to Roll");
+	int start1 = myObj.nextInt();
+	
+	Random rolling = new Random();
+	int rolled = rolling.nextInt(11);
+	rolled++;
+	
+	
+		location1=location1+rolled;
+		System.out.println(location1);
+	
+	
+}
+}
